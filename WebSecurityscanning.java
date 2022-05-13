de70cba24d1db24cc509bedc7f4462ea4abd5b58
Nirmal <43513314+nirmalprojects1988@users.noreply.github.com>
2022-05-13 22:04:23 +0530

    Create WebSecurityscanning.java

---
+++ WebSecurityscanning.java
@@ -0,0 +1,24 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+
+public class WebSecurityscanning{
+
+  
+  @Test
+	public void ecomesecutity() {
+    private WebDriver driver;
+ChromeOptions options=new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		 options.addArguments("--headless");
+		 options.setAcceptInsecureCerts(true);
+    WebDriverManager.chromedriver().setup();
+    driver=new ChromeDriver(options);
+    driver.get("http://automationpractice.com/index.php");
+    driver.quit();
+  }
+}
