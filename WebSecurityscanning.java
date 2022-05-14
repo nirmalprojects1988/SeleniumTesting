ed6f3b0ae9547810e7d97ef05cd863077145e307
Nirmal <43513314+nirmalprojects1988@users.noreply.github.com>
2022-05-14 22:20:28 +0530

    Update WebSecurityscanning.java

--- WebSecurityscanning.java
+++ WebSecurityscanning.java
@@ -12,13 +12,15 @@ public class WebSecurityscanning{
 	public void ecomesecutity() {
     private WebDriver driver;
 ChromeOptions options=new ChromeOptions();
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		 options.addArguments("--headless");
-		 options.setAcceptInsecureCerts(true);
-    WebDriverManager.chromedriver().setup();
-    driver=new ChromeDriver(options);
-    driver.get("http://automationpractice.com/index.php");
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+options.addArguments("--headless");
+options.setAcceptInsecureCerts(true);
+WebDriverManager.chromedriver().setup();
+driver=new ChromeDriver(options);
+driver.get("http://automationpractice.com/index.php");
+String j = driver.getTitle();
+  System.out.println("Your page title Is : "+j);
     driver.quit();
   }
 }
